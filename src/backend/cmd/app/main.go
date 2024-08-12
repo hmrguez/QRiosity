371be55a92f4bdad61ac3f01f69763bf272bab38
hmrguez <zealot.algo@gmail.com>
2024-08-12 14:35:09 -0400

    Added dotenv support

--- src/backend/cmd/app/main.go
+++ src/backend/cmd/app/main.go
@@ -3,9 +3,23 @@ package main
 import (
 	"backend/internal/config"
 	"backend/internal/server"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file", err)
+	}
+
+	// Access the environment variables
+	env := os.Getenv("ENV")
+	llm := os.Getenv("LLM")
+
+	log.Printf("Running in %s environment with LLM: %s", env, llm)
+
 	localConfig := config.NewLocalConfig()
 	server.StartServer(*localConfig)
 }
