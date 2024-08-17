1bccd3d67a573898cc417f9ff8100d6d1098b044
hmrguez <zealot.algo@gmail.com>
2024-08-17 13:46:08 -0400

    Configured frontend to work with new appsync api

--- src/frontend/src/apolloClient.ts
+++ src/frontend/src/apolloClient.ts
@@ -1,9 +1,21 @@
 // src/frontend/src/apolloClient.ts
-import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {ApolloClient, HttpLink, InMemoryCache} from '@apollo/client';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:9000/query',
+	link: new HttpLink({
+		uri: 'https://4sxml7ptwfb7xpbyjbu24jbpnu.appsync-api.us-east-2.amazonaws.com/graphql',  // Replace with your AppSync API URL
+		headers: {
+			'x-api-key': "da2-h4ddio3hgnbtvoz4p5vswbp3fe",  // Replace with your AppSync API key if using API key authentication
+		},
+	}),
 	cache: new InMemoryCache(),
 });
 
+
+// const client = new ApolloClient({
+// 	uri: 'http://localhost:9000/query',
+// 	cache: new InMemoryCache(),
+// });
+
 export default client;
+
