857f18dfd36f2f14343a27b8d4d986d3dff4b2e8
hmrguez <zealot.algo@gmail.com>
2024-09-05 15:15:57 -0400

    Using pointer to allow modifiable response

--- src/backend/internal/services/dailyChallengeService.go
+++ src/backend/internal/services/dailyChallengeService.go
@@ -40,7 +40,7 @@ func (s *DailyChallengeService) GetQuestion(category string) (domain.Problem, er
 	return problem, nil
 }
 
-func (s *DailyChallengeService) RateQuestion(question, answer string) (domain.ChallengeResponse, error) {
+func (s *DailyChallengeService) RateQuestion(question, answer string) (*domain.ChallengeResponse, error) {
 	var challengeResponse domain.ChallengeResponse
 	url := fmt.Sprintf("%s/rate_question", s.baseURL)
 
@@ -50,25 +50,25 @@ func (s *DailyChallengeService) RateQuestion(question, answer string) (domain.Ch
 	}
 	jsonPayload, err := json.Marshal(payload)
 	if err != nil {
-		return challengeResponse, err
+		return nil, err
 	}
 
 	fmt.Println("Url:", url)
 
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonPayload))
 	if err != nil {
-		return challengeResponse, err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return challengeResponse, fmt.Errorf("failed to rate question: %s", resp.Status)
+		return nil, fmt.Errorf("failed to rate question: %s", resp.Status)
 	}
 
 	err = json.NewDecoder(resp.Body).Decode(&challengeResponse)
 	if err != nil {
-		return challengeResponse, err
+		return nil, err
 	}
 
-	return challengeResponse, nil
+	return &challengeResponse, nil
 }
--- src/backend/internal/services/services.go
+++ src/backend/internal/services/services.go
@@ -4,7 +4,7 @@ import "backend/internal/domain"
 
 type IDailyChallengeService interface {
 	GetQuestion(username string) (domain.Problem, error)
-	RateQuestion(question, answer string) (domain.ChallengeResponse, error)
+	RateQuestion(question, answer string) (*domain.ChallengeResponse, error)
 }
 
 type IRoadmapService interface {
